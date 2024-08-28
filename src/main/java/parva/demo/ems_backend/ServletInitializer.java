909b02b19436222f9e937235eac0c5010873b792
raijadaparva <raijadaparva@gmail.com>
2024-08-28 12:50:35 +0530

    init commit

---
+++ src/main/java/parva/demo/ems_backend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package parva.demo.ems_backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmsBackendApplication.class);
+	}
+
+}
---
+++ src/test/java/parva/demo/ems_backend/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package parva.demo.ems_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
